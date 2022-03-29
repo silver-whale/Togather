8897fc99a3532ee22ba5f94d1fad5c37ecdf61b0
silver-whale <garlicwhale@gmail.com>
2022-03-29 17:29:05 +0900

    ListView uid, need to implement search function

--- app/src/main/java/com/example/togather/MainActivity.kt
+++ app/src/main/java/com/example/togather/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.togather
 
+import android.content.ContentProvider
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.util.Log
+import android.widget.AdapterView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
@@ -33,43 +35,45 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, ProfileActivity::class.java))
         }
 
+        search_lv.onItemClickListener = AdapterView.OnItemClickListener{parent, view, position, id ->
+            val selection = parent.getItemAtPosition(position) as UserModel
 
+            Log.i("uid get?", selection.uid)
+        }
 
         database = Firebase.database.reference
-        var usersList = ArrayList<UserModel>()
-        addUserListener(database, usersList)
-        Log.i("UsersList", usersList.toString())
-        val userAdapter = ListAdapter(this, usersList)
-        search_lv.adapter = userAdapter
+
+        addUserListener(database)
     }
 
-    private fun addUserListener(userReference : DatabaseReference, usersList : ArrayList<UserModel>){
+    private fun addUserListener(userReference : DatabaseReference){
+        var usersList = ArrayList<UserModel>()
         val userListener = object : ValueEventListener{
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 //                val user = dataSnapshot.getValue<UserModel>()
-
-
                 val temp = dataSnapshot.getValue() as HashMap<String, HashMap<String, HashMap<String, String>>>
                 val gotUser = temp.get("user")
 
                 for (key in gotUser!!.keys){
                     val getValues = gotUser!!.get(key)
-                    Log.i("tempUser", getValues.toString())
                     val tuid = getValues!!.get("uid")
                     val tname = getValues!!.get("nickname")
                     val ttag : ArrayList<String> = getValues!!.get("hashtag") as ArrayList<String>
                     val tempUser = UserModel(tuid!!, tname!!, ttag)
                     // 객체는 제대로 생성되는 것을 확인함
                     usersList.add(tempUser!!)
+
+                    val userAdapter = ListAdapter(applicationContext, usersList)
+                    search_lv.adapter = userAdapter
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.w("userAdd", "Error")
             }
-
         }
         userReference.addValueEventListener(userListener)
     }
+
 
 }
--- app/src/main/java/com/example/togather/UserModel.kt
+++ app/src/main/java/com/example/togather/UserModel.kt
@@ -6,12 +6,4 @@ data class UserModel(
     var uid : String = "",
     var nickname : String = "",
     var hashtag : ArrayList<String> = ArrayList()
-){
-    fun toMap() : Map<String, Any?>{
-        return mapOf(
-            "uid" to uid,
-            "nickname" to nickname,
-            "hashtag" to hashtag
-        )
-    }
-}
+)
